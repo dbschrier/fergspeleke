9da4c66e6f02a5e88f0d2263ea5d1766a55c2f1c
dbschrier <dbschrier@gmail.com>
2025-04-10 21:05:28 -0400

    updated sortedSquares

--- sortedSquares.py
+++ sortedSquares.py
@@ -1,13 +1,17 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        squares = []
+        res = []
+        L = 0
+        R = len(nums) - 1
 
-        for num in nums:
-            square = num ** 2
-            squares.append(square)
+        while L <= R:
+            if nums[L] ** 2 > nums[R] ** 2:
+                res.append(nums[L] ** 2)
+                L += 1
+            else:
+                res.append(nums[R] ** 2)
+                R -= 1
+        return res[::-1]
 
-        squares.sort()
-        return squares
-
-        #Time Complexity: O(n + however long sort() takes)
+        #Time Complexity: O(n)
         #Space Complexity: O(n) because you create a new array squares.
